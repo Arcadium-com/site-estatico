c1effc61225d6ce5ac8989eab14574b579613710
kaikyuji <kaikykohatsu@gmail.com>
2023-10-15 13:12:38 -0300

    Reorganização dos arquivos

--- Arcadium/site/app.js
+++ Arcadium/site/app.js
@@ -8,17 +8,11 @@ var PORTA = process.env.AMBIENTE_PROCESSO == "desenvolvimento" ? 3333 : 8080;
 
 var app = express();
 
-var indexRouter = require("./src/routes/index");
-var usuarioRouter = require("./src/routes/usuarios");
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(cors());
-
-app.use("/", indexRouter);
-app.use("/usuarios", usuarioRouter);
 
 app.listen(PORTA, function () {
     console.log(`Servidor do seu site já está rodando! Acesse o caminho a seguir para visualizar: http://localhost:${PORTA} \n
